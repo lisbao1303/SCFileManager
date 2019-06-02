c7064bcb37cb1c0a44e1ca4789e014e0c434ba29
thiagao0860 <thiagao0860@gmail.com>
2019-06-02 00:13:59 -0300

    implantacao do navigation draw

--- AutomaticExplorer/app/src/main/java/com/metaconsultoria/root/automaticexplorer/MainActivity.java
+++ AutomaticExplorer/app/src/main/java/com/metaconsultoria/root/automaticexplorer/MainActivity.java
@@ -1,13 +1,18 @@
 package com.metaconsultoria.root.automaticexplorer;
 
+import android.support.annotation.NavigationRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.util.*;
+
 
 public class MainActivity extends AppCompatActivity {
+   // private NavigationR mNavigationDrawerFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }  // teste de inicio do programa
+    }
 }
