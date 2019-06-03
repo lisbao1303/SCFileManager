0706709a60fbb2415604dd6ee293974dbfc5ee77
thiagao0860 <thiagao0860@gmail.com>
2019-06-03 19:17:00 -0300

    Navigation Drawer e Interface de Entrada "Funcionando" mas precisa melhorar muito em interface

--- SCFileManager/app/src/main/java/com/metaconsultoria/root/scfilemanager/MainNavigationDrawerActivity.java
+++ SCFileManager/app/src/main/java/com/metaconsultoria/root/scfilemanager/MainNavigationDrawerActivity.java
@@ -1,9 +1,6 @@
 package com.metaconsultoria.root.scfilemanager;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -12,6 +9,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainNavigationDrawerActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -22,15 +20,6 @@ public class MainNavigationDrawerActivity extends AppCompatActivity
         setContentView(R.layout.activity_main_navigation_drawer);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -81,7 +70,7 @@ public class MainNavigationDrawerActivity extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.nav_camera) {
-            // Handle the camera action
+            Toast.makeText(this,"Action Import",Toast.LENGTH_SHORT).show();
         } else if (id == R.id.nav_gallery) {
 
         } else if (id == R.id.nav_slideshow) {
