f2eacd886765b39c58493f9c85d29c0ec6f7432d
thiagao0860 <thiagao0860@gmail.com>
2019-06-05 06:17:49 -0300

    Mechendo em interface denovo

--- SCFileManager/app/src/main/java/com/metaconsultoria/root/scfilemanager/MainNavigationDrawerActivity.java
+++ SCFileManager/app/src/main/java/com/metaconsultoria/root/scfilemanager/MainNavigationDrawerActivity.java
@@ -95,6 +95,7 @@ public class MainNavigationDrawerActivity extends AppCompatActivity
 
         }
 
+
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
